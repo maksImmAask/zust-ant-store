9ee6951734c9399714f8e7ae16b293066dc5ec87
Maksim Azamatov <madina@192.168.1.12>
2025-06-23 21:01:48 +0500

    q

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import './App.css'
-import { Layout, Row, Col, Button, Carousel} from 'antd'
+import { Layout, Row, Col, Button, Carousel, notification } from 'antd'
 import {
   ShoppingCartOutlined,
   ShopOutlined
 } from '@ant-design/icons';
-const { Header, Content, Footer} = Layout
+const { Header, Content} = Layout
 
 function App() {
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = () => {
+    api.open({
+      message: 'Ой...',
+      description:
+        'Эта функция еще не работает...',
+      duration: 0,
+    });
+  };
   return (
     <Layout className="layout">
       <Header className="header">
         <div className="container">
           <Row style={{ width: '100%', height: '100%' }}>
             <Col span={2} className='col'>
-              <Button style={{ color: 'white', fontSize: '20px', border: 'none', background: 'transparent' }}>UStore</Button>
+              <Button onClick={openNotification} style={{ color: 'white', fontSize: '20px', border: 'none', background: 'transparent' }}>UStore</Button>
             </Col>
             <Col span={2} className='col' style={{alignItems: 'center', display: 'flex', justifyContent: 'center'}}>
               <ShopOutlined style={{color: 'white', fontSize: '30px'}}/>
@@ -21,14 +30,15 @@ function App() {
             <Col span={4} className='col'></Col>
             <Col span={4} className='col'></Col>
             <Col span={4} className='col'>
-              <Button style={{ color: 'white', fontSize: '20px', border: 'none', background: 'transparent' }}>Избранное</Button>
+              <Button onClick={openNotification} style={{ color: 'white', fontSize: '20px', border: 'none', background: 'transparent' }}>Избранное</Button>
             </Col>
             <Col span={3} className='col' style={{alignItems: 'center', display: 'flex', justifyContent: 'center'}}>
-              <Button style={{border: 'none', background: 'transparent'}}><ShoppingCartOutlined style={{color: 'white', fontSize: '30px'}}/></Button>
+              {contextHolder}
+              <Button onClick={openNotification} style={{border: 'none', background: 'transparent'}}><ShoppingCartOutlined style={{color: 'white', fontSize: '30px'}}/></Button>
             </Col>
             <Col span={5} className='col'>
-             <Button type="primary">Log In</Button>
-             <Button style={{color: 'white'}} >Sign In</Button>
+             <Button style={{background: 'transparent'}} onClick={openNotification} type="primary">Log In</Button>
+             <Button style={{background: 'transparent', color: 'white'}} onClick={openNotification} >Sign In</Button>
             </Col>
           </Row>
         </div>
